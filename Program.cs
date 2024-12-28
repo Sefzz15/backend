7ec69ce707dbc9b18ebab4eb0acf3c09e7c6eea5
Sefzz <57561143+Sefzz15@users.noreply.github.com>
2024-12-28 18:03:06 +0200

    implemented a succesful connection with the front end app and the MySQL Server.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using backend.Data;
+using MySql.Data.MySqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +18,26 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.UseCors(builder =>
+    builder.WithOrigins("http://localhost:4200") // Angular URL
+           .AllowAnyHeader()
+           .AllowAnyMethod());
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+// try
+// {
+//     using (MySqlConnection conn = new MySqlConnection(connectionString))
+//     {
+//         conn.Open();
+//         Console.WriteLine("Η σύνδεση με τη βάση δεδομένων είναι επιτυχής.");
+//     }
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"Σφάλμα κατά τη σύνδεση: {ex.Message}");
+// }
