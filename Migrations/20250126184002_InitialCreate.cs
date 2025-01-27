33908f6c055b560313a44e469c4b29d097041545
Sefzz15 <arsserafim98@gmail.com>
2025-01-27 20:55:05 +0200

    Update project paths and assembly information for user environment consistency

--- Migrations/20250126184002_InitialCreate.cs
+++ Migrations/20250126184002_InitialCreate.cs
@@ -7,7 +7,7 @@ using MySql.EntityFrameworkCore.Metadata;
 namespace backend.Migrations
 {
     /// <inheritdoc />
-    public partial class initialcreate : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
