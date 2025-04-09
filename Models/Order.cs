9831d1879b97121ffe62562c24ec83b4697ec1af
Sefzz15 <arsserafim98@gmail.com>
2025-04-09 19:37:09 +0300

    Refactor UserController to include JwtService for token generation; update Order model formatting for consistency

--- Models/Order.cs
+++ Models/Order.cs
@@ -6,14 +6,14 @@ public class Order
     [Key]
     public int Oid { get; set; } // Primary key
 
-        public int Ooid { get; set; }
+    public int Ooid { get; set; }
 
     public int Cid { get; set; } // Customer ID
     public DateTime Date { get; set; }
 
-      // Navigation property for order items
-  
-   [JsonIgnore]
+    // Navigation property for order items
+
+    [JsonIgnore]
     public Customer? Customer { get; set; }
     [JsonIgnore]
     public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
